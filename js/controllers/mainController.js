ad6570fa7c8e7e1b188af4dafed80d127f115f98
Markaryan Zhirayr <zhirayr82@hotmail.fr>
2017-02-28 17:55:29 +0100

    Angular Project

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,16 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+	$scope.currentPersonId = -1;
+	$scope.person = [];
+	$scope.addPerson = function () {
+		$scope.person.push({
+			id: $scope.person.length+1,
+			lastName: $scope.lastName,
+			firstName: $scope.firstName,
+			mail: $scope.mail
+		}); 
+	};
+	$scope.deletePerson = function(id) {
+		$scope.person.splice( id, 1 );
+	}
 }
